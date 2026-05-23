e7257d9cfd219b88e8ba890441a5ed227eb97f14
emerald32-source <emerald320032@gmail.com>
2026-05-23 05:09:32 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,55 @@
+const clientCustomName = "قطقوطه";
+const clientGender = "f";
+const clientDate = "2026-05-23";
+const clientEnvelopeText = `ميااو مياو ميااااو مياو ميااااااااو مياو ميااااااااو مياو ميااااااااو مياو ميااااو مياو ميااااو مياو ميااااو مياو ميااااااااو مياو ميااااااااو مياو ميااااااااو مياو ميااااو مياو ميااااو مياععععو 
+مياو مياو❤️`;
+const clientMemories = [
+    {
+        "photo": "photo1.jpg",
+        "caption": "مياو حبيبتي"
+    },
+    {
+        "photo": "photo2.jpg",
+        "caption": "مياو قطتي"
+    },
+    {
+        "photo": "photo3.jpg",
+        "caption": "مياو و انتي طيبه"
+    },
+    {
+        "photo": "photo4.jpg",
+        "caption": "مياو عليكي بالخير"
+    },
+    {
+        "photo": "photo5.jpg",
+        "caption": "كل مياو حياتي"
+    },
+    {
+        "photo": "photo6.jpg",
+        "caption": "مياو ميااااو"
+    },
+    {
+        "photo": "photo7.jpg",
+        "caption": "مياو الكيوت"
+    },
+    {
+        "photo": "photo8.jpg",
+        "caption": "مياو جدا"
+    },
+    {
+        "photo": "photo9.jpg",
+        "caption": "قطتي بئا"
+    },
+    {
+        "photo": "photo10.jpg",
+        "caption": "روحي التامنه"
+    },
+    {
+        "photo": "photo11.jpg",
+        "caption": "كل ارواحي السابعه"
+    },
+    {
+        "photo": "photo12.jpg",
+        "caption": "امياو فيكي"
+    }
+];
